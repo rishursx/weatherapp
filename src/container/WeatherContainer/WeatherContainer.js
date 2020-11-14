4fece7d71b68fe0163416d1844432daa9ed47102
Rishu Trivedi <rishu.trivedi1997@gmail.com>
2020-11-14 18:27:33 +0530

    added icon & text

--- src/container/WeatherContainer/WeatherContainer.js
+++ src/container/WeatherContainer/WeatherContainer.js
@@ -8,7 +8,10 @@ import { TextField, Typography } from "@material-ui/core";
 const WeatherContainer = () => {
   const [weatherData, setWeatherData] = useState({
     current_temperature_celsius: "",
+    current_condition_icon: "",
+    current_conditon_text: "",
     current_location: "",
+    selected_unit: "Celcius",
   });
 
   useEffect(() => {
@@ -17,6 +20,8 @@ const WeatherContainer = () => {
       setWeatherData({
         current_temperature_celsius: response.data.current.temp_c,
         current_location: `${response.data.location.name}, ${response.data.location.country}`,
+        current_condition_icon: response.data.current.condition.icon,
+        current_condition_text: response.data.current.condition.text,
       });
     });
   }, []);
@@ -29,6 +34,7 @@ const WeatherContainer = () => {
           <TextField
             variant="outlined"
             label="Location"
+            placeholder="Enter City Name/ Zip Code"
             onChange={(event) => {
               setWeatherData({
                 ...weatherData,
@@ -39,10 +45,19 @@ const WeatherContainer = () => {
         </div>
         <div className={classes.currentWeatherPane}>
           <Typography variant="h1">
-            {weatherData.current_temperature_celsius}
+            {weatherData.current_temperature_celsius}°
           </Typography>
           <Typography variant="subtitle1">
             {weatherData.current_location}
+          </Typography>
+        </div>
+        <div className={classes.extraInfo}>
+          <img
+            src={weatherData.current_condition_icon}
+            alt={weatherData.current_condition_text}
+          />
+          <Typography variant="subtitle1">
+            {weatherData.current_condition_text}
           </Typography>
         </div>
       </div>
